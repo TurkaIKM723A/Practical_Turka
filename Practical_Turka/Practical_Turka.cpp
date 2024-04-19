55f26014926f5db50b9c00d423f48d2102386ddd
Jambi <iturkas42@gmail.com>
2024-04-19 16:14:49 +0300

    Add project files.

---
+++ Practical_Turka/Practical_Turka.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b) {
+
+    return a + b;
+
+}
+
+int main() {
+
+    setlocale(LC_CTYPE, "ukr");
+
+    int num1, num2;
+
+    cout << "¬ведiть перше  число: ";
+    cin >> num1;
+
+    cout << "¬ведiть друге число: ";
+    cin >> num2;
+
+    cout << "—ума чисел " << num1 << " i " << num2 << " дорiвнюЇ: " << sum(num1, num2) << '\n';
+
+    return 0;
+}
